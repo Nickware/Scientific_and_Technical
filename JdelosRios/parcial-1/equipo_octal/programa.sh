6d764cfabadaa666f24bb5dd3bede69d5c17c45f
juandelosrios21 <rmahechad@correo.udistrital.edu.co>
2021-09-08 15:30:07 -0500

    modificando programa.sh

---
+++ JdelosRios/parcial-1/equipo_octal/programa.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#parcial-1#
+echo "ultimo digito del codigo institucional:"
+echo "1"
+echo "ultimo digito del documento personal"
+echo "8"
+echo "\0033[30;47mPrograma Turing\0033[0m"
+echo "\0033[37;40mEquipo Octal\0033[0m"
+
+
+
+
