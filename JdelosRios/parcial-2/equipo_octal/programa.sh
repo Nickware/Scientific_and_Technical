312c350ae4426cee27c33929c8337cd7fc2cb7f7
juandelosrios21 <rmahechad@correo.udistrital.edu.co>
2021-09-08 17:48:04 -0500

    modificando sh

--- JdelosRios/parcial-2/equipo_octal/programa.sh
+++ JdelosRios/parcial-2/equipo_octal/programa.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 #parcial-1#
 echo "Juan David de los Rios Mahecha"
+sleep 3
+cat problema.txt
+sleep 10
 echo "ultimo digito del codigo institucional:"
 echo "1"
 echo "ultimo digito del documento personal"
 echo "8"
 echo "\0033[30;47mPrograma Turing\0033[0m"
+sleep 3
 echo "\0033[37;40mEquipo Octal\0033[0m"
 echo "Funcion del programa Turing"
+sleep 2
 echo "Cifrar y decifrar mensajes de texto"
 echo "cambiando permisos"
 chmod 740 programa.sh
- 
-
-
-
+echo "Crendo ad.txt"
+sleep 2 
+echo "En este archivo.txt debera escribir el mensaje que desea decifrar"
+touch ad.txt
+ls
+echo "a continuacion de muestra una interfaz en la que debera escribir el mensaje de texto que desea decifrar"
+chmod 777 ad.txt
+sleep 30
+nano ad.txt
+sleep 2
+cat ad.txt
 
