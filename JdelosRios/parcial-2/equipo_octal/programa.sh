1d0c8b8ea1e875594e3d134d84c6cd08c94576e6
juandelosrios21 <rmahechad@correo.udistrital.edu.co>
2021-09-08 16:39:58 -0500

    renombrando archivos

---
+++ JdelosRios/parcial-2/equipo_octal/programa.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#parcial-1#
+echo "Juan David de los Rios Mahecha"
+echo "ultimo digito del codigo institucional:"
+echo "1"
+echo "ultimo digito del documento personal"
+echo "8"
+echo "\0033[30;47mPrograma Turing\0033[0m"
+echo "\0033[37;40mEquipo Octal\0033[0m"
+echo "Funcion del programa Turing"
+echo "Cifrar y decifrar mensajes de texto"
+echo "cambiando permisos"
+chmod 740 programa.sh
+ 
+
+
+
+
