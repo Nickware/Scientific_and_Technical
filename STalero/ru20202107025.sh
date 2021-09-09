f9bd55b1b2da7d9f278be7152648b8ac3d96756f
SantiTalero042 <santialejo156@gmail.com>
2021-09-09 01:19:58 -0500

    Parcial 1

---
+++ STalero/ru20202107025.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/bash.exe
+#title: Rutina de Descompresion
+#author: Santiago Talero Parra
+#date: 08/09/2021
+#version: 0.0.1
+echo Bienvenido a la rutina de descompresion de archivos
+echo confirme su identidad:
+read nombre
+
+echo Bienvenido: 
+echo Ejecutando descompresion
+tar -xf Pro20202107025.tar
+echo Proceso finalizado
