99653cf6d5a2ff762d25e93c8c99c1e6f378fbbd
LauraOliveros <lvoliverosm@correo.udistrital.edu.co>
2021-09-09 12:07:37 -0500

    Subir parcial, equipo hexal

---
+++ LOliveros/ru20202107010.sh
@@ -0,0 +1,110 @@
+title: Equipo Hexal de Turing
+author: Laura Oliveros
+data: 06/09/21
+version: 0.0.1
+Datos básicos del desarrollador
+Último dígito del código estudiantil: 0
+Último dígito del documento de identificación: 1
+Primero se crea una carpeta usando el comando *mkdir, esta se debe nombrar con la primera letra de su nombre y su primer apellido, por ejemplo:  
+Luego se crea un archivo con extención.sh con el comando *touch, se recomienda nombrarlo con la abreviación bi de bitácora y con su código, por ejemplo:  y se le dan los correspondientes permisos con el comando: 
+Ahora se entra en la bitácora y se le dan los datos requeridos. Es buena costumbre nombrarlos con #title, #author, #data y #version
+Después se crean dos archivos de texto: el de entrada  y el de salida , y se le dan los permisos respectivos a cada uno:  y  
+Ahora se crea el archivo ejecutable con extención.run:  y con permisos 
+Tambien se le dan los datos requeridos a cada uno de los archivos como: #title, #author, #data y #version y los datos del desarrollador
+Se crea la presente rutina con los datos ya usados en los archivos anteriores y mismor permisos
+Se escribe el mensaje sin cifrar, el código que se usó para cifrar y se direcciona al texto de entrada
+Se utiliza el código: 70616C616272610A para cifrar, se reemplaza la variable palabra por el texto y se direcciona primeramente al texto de salida y despues al ejecutable
+Se escoge el presente código de cifrado por facilidad para el lector, es comodo de usar, organizado y ocupa menos espacio que el cifrado de forma vertical
+title: Equipo Hexal de Turing
+author: Laura Oliveros
+data: 06/09/21
+version: 0.0.1
+Datos básicos del desarrollador
+Último dígito del código estudiantil: 0
+Último dígito del documento de identificación: 1
+Primero se crea una carpeta usando el comando *mkdir, esta se debe nombrar con la primera letra de su nombre y su primer apellido, por ejemplo:  
+Luego se crea un archivo con extención.sh con el comando *touch, se recomienda nombrarlo con la abreviación bi de bitácora y con su código, por ejemplo:  y se le dan los correspondientes permisos con el comando: 
+Ahora se entra en la bitácora y se le dan los datos requeridos. Es buena costumbre nombrarlos con #title, #author, #data y #version
+Después se crean dos archivos de texto: el de entrada  y el de salida , y se le dan los permisos respectivos a cada uno:  y  
+Ahora se crea el archivo ejecutable con extención.run:  y con permisos 
+Tambien se le dan los datos requeridos a cada uno de los archivos como: #title, #author, #data y #version y los datos del desarrollador
+Se crea la presente rutina con los datos ya usados en los archivos anteriores y mismor permisos
+Se escribe el mensaje sin cifrar, el código que se usó para cifrar y se direcciona al texto de entrada
+Se utiliza el código: 70616C616272610A para cifrar, se reemplaza la variable palabra por el texto y se direcciona primeramente al texto de salida y despues al ejecutable
+Se escoge el presente código de cifrado por facilidad para el lector, es comodo de usar, organizado y ocupa menos espacio que el cifrado de forma vertical
+title: Equipo Hexal de Turing
+author: Laura Oliveros
+data: 06/09/21
+version: 0.0.1
+Datos básicos del desarrollador
+Último dígito del código estudiantil: 0
+Último dígito del documento de identificación: 1
+Primero se crea una carpeta usando el comando *mkdir, esta se debe nombrar con la primera letra de su nombre y su primer apellido, por ejemplo:  
+Luego se crea un archivo con extención.sh con el comando *touch, se recomienda nombrarlo con la abreviación bi de bitácora y con su código, por ejemplo:  y se le dan los correspondientes permisos con el comando: 
+Ahora se entra en la bitácora y se le dan los datos requeridos. Es buena costumbre nombrarlos con title, author, data y version
+Después se crean dos archivos de texto: el de entrada  y el de salida , y se le dan los permisos respectivos a cada uno:  y  
+Ahora se crea el archivo ejecutable con extención.run:  y con permisos 
+Tambien se le dan los datos requeridos a cada uno de los archivos como: title, author, data y version y los datos del desarrollador
+Se direcciona los dos textos y el ejecutable al archivo de compresión Pro20202107010.zip
+Se crea la presente rutina con los datos ya usados en los archivos anteriores y mismor permisos
+Se escribe el mensaje sin cifrar, el código que se usó para cifrar y se direcciona al texto de entrada
+Se utiliza el código: 70616C616272610A para cifrar, se reemplaza la variable palabra por el texto y se direcciona primeramente al texto de salida y despues al ejecutable
+Se escoge el presente código de cifrado por facilidad para el lector, es comodo de usar, organizado y ocupa menos espacio que el cifrado de forma vertical
+Por último se ejecutan los archivos en la terminal de la siguiente forma: primero la bitácora y después el ejecutable de extensión.run
+title: Equipo Hexal de Turing
+author: Laura Oliveros
+data: 06/09/21
+version: 0.0.1
+Datos básicos del desarrollador
+Último dígito del código estudiantil: 0
+Último dígito del documento de identificación: 1
+Primero se crea una carpeta usando el comando *mkdir, esta se debe nombrar con la primera letra de su nombre y su primer apellido, por ejemplo:  
+Luego se crea un archivo con extención.sh con el comando *touch, se recomienda nombrarlo con la abreviación bi de bitácora y con su código, por ejemplo:  y se le dan los correspondientes permisos con el comando: 
+Ahora se entra en la bitácora y se le dan los datos requeridos. Es buena costumbre nombrarlos con title, author, data y version
+Después se crean dos archivos de texto: el de entrada  y el de salida , y se le dan los permisos respectivos a cada uno:  y  
+Ahora se crea el archivo ejecutable con extención.run:  y con permisos 
+Tambien se le dan los datos requeridos a cada uno de los archivos como: title, author, data y version y los datos del desarrollador
+Se direcciona los dos textos y el ejecutable al archivo de compresión Pro20202107010.zip
+Se crea la presente rutina con los datos ya usados en los archivos anteriores y mismor permisos
+Se escribe el mensaje sin cifrar, el código que se usó para cifrar y se direcciona al texto de entrada
+Se utiliza el código: 70616C616272610A para cifrar, se reemplaza la variable palabra por el texto y se direcciona primeramente al texto de salida y despues al ejecutable
+Se escoge el presente código de cifrado por facilidad para el lector, es comodo de usar, organizado y ocupa menos espacio que el cifrado de forma vertical
+Por último se ejecutan los archivos en la terminal de la siguiente forma: primero la bitácora y después el ejecutable de extensión.run
+title: Equipo Hexal de Turing
+author: Laura Oliveros
+data: 06/09/21
+version: 0.0.1
+Datos básicos del desarrollador
+Último dígito del código estudiantil: 0
+Último dígito del documento de identificación: 1
+Primero se crea una carpeta usando el comando *mkdir, esta se debe nombrar con la primera letra de su nombre y su primer apellido, por ejemplo:  
+Luego se crea un archivo con extención.sh con el comando *touch, se recomienda nombrarlo con la abreviación bi de bitácora y con su código, por ejemplo:  y se le dan los correspondientes permisos con el comando: 
+Ahora se entra en la bitácora y se le dan los datos requeridos. Es buena costumbre nombrarlos con title, author, data y version
+Después se crean dos archivos de texto: el de entrada  y el de salida , y se le dan los permisos respectivos a cada uno:  y  
+Ahora se crea el archivo ejecutable con extención.run:  y con permisos 
+Tambien se le dan los datos requeridos a cada uno de los archivos como: title, author, data y version y los datos del desarrollador
+Se direcciona los dos textos y el ejecutable al archivo de compresión Pro20202107010.zip
+Se crea la presente rutina con los datos ya usados en los archivos anteriores y mismor permisos
+Se escribe el mensaje sin cifrar, el código que se usó para cifrar y se direcciona al texto de entrada
+Se utiliza el código: 70616C616272610A para cifrar, se reemplaza la variable palabra por el texto y se direcciona primeramente al texto de salida y despues al ejecutable
+Se escoge el presente código de cifrado por facilidad para el lector, es comodo de usar, organizado y ocupa menos espacio que el cifrado de forma vertical
+Por último se ejecutan los archivos en la terminal de la siguiente forma: primero la bitácora y después el ejecutable de extensión.run
+title: Equipo Hexal de Turing
+author: Laura Oliveros
+data: 06/09/21
+version: 0.0.1
+Datos básicos del desarrollador
+Último dígito del código estudiantil: 0
+Último dígito del documento de identificación: 1
+Primero se crea una carpeta usando el comando *mkdir, esta se debe nombrar con la primera letra de su nombre y su primer apellido, por ejemplo:  
+Luego se crea un archivo con extención.sh con el comando *touch, se recomienda nombrarlo con la abreviación bi de bitácora y con su código, por ejemplo:  y se le dan los correspondientes permisos con el comando: 
+Ahora se entra en la bitácora y se le dan los datos requeridos. Es buena costumbre nombrarlos con title, author, data y version
+Después se crean dos archivos de texto: el de entrada  y el de salida , y se le dan los permisos respectivos a cada uno:  y  
+Ahora se crea el archivo ejecutable con extención.run:  y con permisos 
+Tambien se le dan los datos requeridos a cada uno de los archivos como: title, author, data y version y los datos del desarrollador
+Se direcciona los dos textos y el ejecutable al archivo de compresión Pro20202107010.zip
+Se crea la presente rutina con los datos ya usados en los archivos anteriores y mismor permisos
+Se escribe el mensaje sin cifrar, el código que se usó para cifrar y se direcciona al texto de entrada
+Se utiliza el código: 70616C616272610A para cifrar, se reemplaza la variable palabra por el texto y se direcciona primeramente al texto de salida y despues al ejecutable
+Se escoge el presente código de cifrado por facilidad para el lector, es comodo de usar, organizado y ocupa menos espacio que el cifrado de forma vertical
+Por último se ejecutan los archivos en la terminal de la siguiente forma: primero la bitácora y después el ejecutable de extensión.run
