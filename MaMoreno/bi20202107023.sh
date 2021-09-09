e1aec9819cb4f3f81cfcaa8ff9792dc0dbf0e04e
Almarm-r <maamorenor@correo.udistrital.edu.co>
2021-09-09 03:57:29 -0500

    Subiendo parcial

---
+++ MaMoreno/bi20202107023.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+#Author María Moreno 
+
+#Date 21-09-09
+#Version 1.0.0
+#crear ejecutable .run 
+touch ejecutable.sh
+echo -e " #Author: María Alejandra Moreno (cod_end = 3; cc_end =3)">> ejecutable.sh
+echo -e "#Date 21-09-09 ">>ejecutable.sh
+echo -e "#Version 1.0.0">> ejecutable.sh
+#Para pasar información desde un .txt
+#hexdump -c Mensaje_.txt >> salida.txt # Así puedo pasar a ASCCI el mensaje.
+
+
+#while IFS= read -r line; do
+#  printf '%s\n' "$line"
+#done < mensaje_codificado.txt  ---> no concatena como necesito :(
+
+#------->>ejecutable.run para pasar la información final al archivo ejecutable----
+echo -e "line=$(cat -v imensajeascci.txt | sed 's/\'^M'//gI')" >> ejecutable.sh  #---> utiliza sed para concatenar los espacio y sacar la informacion linea a linea 
+echo -e "line_bi=$(cat -v imensajebin.txt | sed 's/\'^M'//gI')"  >> ejecutable.sh 
+echo -e "line_oct=$(cat -v imensajeoct.txt | sed 's/\'^M'//gI')">> ejecutable.sh
+#Estas funciones son para convertir los caracteres
+# chrbin entran vinarios salen octales toma el primer argumento $1 ($1 significa primer argumentodel file https://bash.cyberciti.biz/guide/$1 )
+# el // significa "Displays a backslash character"
+
+echo -e "
+chrbin() {
+    echo $(printf \\$(echo "ibase=2; obase=8; "'$1'"" | bc))
+} " >> ejecutable.sh
+echo -e "
+ordbin() {
+  a=$(printf '%d' '$1' )            
+  echo "obase=2; '$a'" | bc           
+}" >> ejecutable.sh
+echo -e " 
+ordoct(){
+   a=$(printf '%d' '$1') 
+    echo "obase=8; '$a'" | bc
+}" >> ejecutable.sh
+#--->asigancion decimal para encontrar su equivalente
+#---> convierte a decimal a donde a es la impresion "printf" con conversion de especificacion
+# "%d" que optine un entero decimal con signo. https://linuxize.com/post/bash-printf-command/
+
+  
+# ---> input field separator "IFS"---- $* es parametro especial para conectar espacios "concatenar"
+#-- tr es un metodo para eliminar caracteres de  strings donde tr -d elimina los caracteres definidos
+echo -e "
+ function textobinario () {
+    echo -n "'$*'" | while IFS= read -r -n1 char 
+    do
+        ordbin "'$char'" | tr -d "'`\n`'"
+    
+        echo -n " "
+    done
+} " >> ejecutable.sh 
+echo -e "
+function binariotexto () {
+    for bin in "'$*'" 
+    do
+        chrbin "'$bin'" | tr -d "'\n'"
+        
+    done
+}">> ejecutable.sh 
+#Siguendo la misma logica para cifrar en octal utilizar ordoctal donde se cambio la base de salida obase por 8. 
+echo -e "function textooctal () {
+    echo -n "'$*'" | while IFS= read -r -n1 char2 
+    do
+        ordoct  "'$char2'"| tr -d "'\n'"
+    
+        echo -n " "
+    done
+}" >>ejecutable.sh 
+
+#creando archivos de salida 
+echo -e "touch textobinario.txt textooctal.txt binariotexto.txt">>ejecutable.sh 
+
+echo -e "textobinario '$line'>>textotextobinario.txt" >>ejecutable.sh 
+echo -e "binariotexto '$line_bi'>>binariotexto.txt">>ejecutable.sh 
+echo -e "textooctal '$line_oct' >>textooctal.txt">> ejecutable.sh 
+# Yupi!!!... por fin funciono!!!
+#Ultimando detalles del la bitacora 
+#Asignar permisos "tener permisos de lectura para los usuarios de su equipo; lectura,tener permisos de lectura para los usuarios de su equipo; lectura,"
+#chmood 760 bi20202107023.run 
+
+
+#touch ru20202107023.sh  
+
+
+
+
+
+
