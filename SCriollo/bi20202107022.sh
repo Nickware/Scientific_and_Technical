8c75b7cbd80719e4824d59a629e636dc4ddec1f0
Scriollob <scriollob@correo.udistrital.edu.co>
2021-09-09 17:47:36 -0500

    Prueba 1 de script: fallo en la lectura

--- SCriollo/bi20202107022.sh
+++ SCriollo/bi20202107022.sh
@@ -2,9 +2,19 @@
 #titulo: Bitácora
 #autor: Santiago Criollo Bermudez
 #fecha: 09/09/2021
-#version: 0.0.1
+#version: 0.0.2
 #codigo: 2
 #identificacion: 1
+
+echo "Año: 1940"
+echo "Programa para cifrar numeros hexagecimales" >> salida.txt
+echo "Recuerde que las tropas nazis avanzan hacia Paris, su mensaje puede salvar vidas"
+echo -e \ "ingrese el texto que quiere cifrar:" >> salida.txt
+read $texto >> salida.txt
+echo "Codificando su mensaje, espere por favor" >> salida.txt
+echo "Su mensaje en hexagecimales:" >> salida.txt
+echo $texto | hexdump >> salida.txt
+echo $texto | hexdump
 
 
 
