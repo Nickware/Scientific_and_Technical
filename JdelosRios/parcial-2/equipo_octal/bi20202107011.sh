a8ae4c0a08fd16436fb01eb3d37121b55bdf3b87
juandelosrios21 <rmahechad@correo.udistrital.edu.co>
2021-09-09 11:24:07 -0500

    modificando archivos final

--- JdelosRios/parcial-2/equipo_octal/bi20202107011.sh
+++ JdelosRios/parcial-2/equipo_octal/bi20202107011.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #parcial-1#
-cp programa.sh programa.run
+cp bi20202107011.sh programa.run
 echo "Juan David de los Rios Mahecha"
 sleep 3
 cat problema.txt
@@ -16,7 +16,7 @@ echo "Funcion del programa Turing"
 sleep 2
 echo "Cifrar mensajes de texto"
 echo "cambiando permisos"
-chmod 740 prueba.sh
+chmod 740 bi20202107011.sh
 echo "\0033[30;47mCodigo de Prueba\0033[0m"
 echo "codigo sin cifrar:"
 cat prueba.txt
@@ -28,7 +28,7 @@ echo "En este archivo.txt debera escribir el mensaje que desea cifrar"
 touch ad.txt
 echo "listando archivos"
 ls
-echo "a continuacion de muestra una interfaz en la que debera escribir el mensaje de texto que desea decifrar"
+echo "a continuacion de muestra una interfaz en la que debera escribir el mensaje de texto que desea cifrar"
 chmod 777 ad.txt
 sleep 30
 nano ad.txt
@@ -40,13 +40,5 @@ rm ad.txt
 od -b codigo.txt |tee -a historial.txt
 rm codigo.txt
 chmod 777 historial.txt
-touch Pro20202107011.tar
-echo "listando archivos"
-ls
-echo "comprimiendo archivos"
-tar -rvf Pro20202107011.tar programa.sh
-tar -rvf Pro20202107011.tar programa.run 
-tar -rvf Pro20202107011.tar historial.txt 
-tar -rvf Pro20202107011.tar prueba.txt
-
+sh programa.run 
                         
