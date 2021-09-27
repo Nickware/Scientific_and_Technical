85958365e1d051a395538ae638f0f2f23c55ebcb
LauraOliveros <lvoliverosm@correo.udistrital.edu.co>
2021-09-27 11:22:44 -0500

    Creando archivo variables LOliveros

---
+++ LOliveros/LO270921/variables.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/bash.exe
+#author: Laura Oliveros
+#data: 27/09/21
+#version: 0.0.1
+
+# variables - constantes - array
+
+# Ejemplo 1
+# export TZ=America/Bogota
+# echo "Zona horaria es: $TZ"
+# date
+# export TZ=Asia/Tokyo
+# echo "Zona horaria es: $TZ"
+# date
+# unset TZ 
+
+# Ejemplo 2
+# echo "Total numero de parametros son: $#"
+# echo "Nombre del archivo: $0"
+# echo "Primer parametro: $1"
+# echo "Segundo parametro: $2"
+# echo "Tercer parametro: $3"
+# echo "Todo los parametros: $*"
