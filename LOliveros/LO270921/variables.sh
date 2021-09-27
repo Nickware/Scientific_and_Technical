b698adc5937a964d17275e604ec0a8389d225f43
LauraOliveros <lvoliverosm@correo.udistrital.edu.co>
2021-09-27 12:14:13 -0500

    Trabajando con variables

--- LOliveros/LO270921/variables.sh
+++ LOliveros/LO270921/variables.sh
@@ -21,3 +21,30 @@
 # echo "Segundo parametro: $2"
 # echo "Tercer parametro: $3"
 # echo "Todo los parametros: $*"
+
+# Ejemplo 3
+# echo "La fecha es: $(date)"
+# set $(date)
+# echo El mes es: $3
+# exit 0
+
+# Ejemplo 4
+# echo "Todos los argumentos "
+# echo "$*"
+# echo "Shift, en la primera posicion"
+# Shift
+# echo 1
+# Shift 
+# echo "Shift, en la segunda posicion"
+# Shift 2
+# echo "Mejorar ejemplo 4"
+
+# Ejemplo 5
+
+# python - octave - matlab - julia - R 
+# c, c++, java
+# frutas=(aguacate banano manzana)
+# echo ${frutas[*]}
+# echo ${frutas[0]}
+
+# arreglo=(x y z)
