5c8821a8c12515ef9532692d003f5f91023dc318
NotaT3chie <marirodriguezp@correo.udistrital.edu.co>
2021-09-30 00:37:45 -0500

    Moving

--- MRodríguezExam/ru20202107005.sh
+++
@@ -1,9 +0,0 @@
-echo "#Coding Nazi message"
-echo "#Mariana Rodríguez Perdomo"
-echo "#09/09/2021"
-echo "#v 0.2"
-echo "#ID ending at 053"
-echo "#StudentCode ending at 005"
-
-unzip pro20202107005.zip
-
