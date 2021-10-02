04fef415b7af21df5de27402900418157d34d484
NotaT3chie <marirodriguezp@correo.udistrital.edu.co>
2021-10-02 12:02:39 -0500

    Adding my file

---
+++ MRodríguez/MRodríguezExam/ru20202107005.sh
@@ -0,0 +1,9 @@
+echo "#Coding Nazi message"
+echo "#Mariana Rodríguez Perdomo"
+echo "#09/09/2021"
+echo "#v 0.2"
+echo "#ID ending at 053"
+echo "#StudentCode ending at 005"
+
+unzip pro20202107005.zip
+
---
+++ MRodríguez/WorkClass1/bitacora.sh
@@ -0,0 +1,23 @@
+#Commnting on
+
+echo "echo "Deleting files .txt"" >> 20202107005.run
+echo "To delete files: "
+echo "rm example.txt"
+echo "rm sç.txt"
+echo "Deleting example.txt and sç.txt"
+rm example.txt
+rm sç.txt
+echo "Your files were deleted successfully"
+
+#Requesting information
+echo -n "Give me your name: "
+echo "echo -n "Give me your name: "" >> 20202107005.run
+read name
+echo "read name" >> 20202107005.run
+
+echo "Thanks, you entered the name $name" 
+echo "echo "Thanks, you entered the name $name" >> output.txt"
+#Tranfering the info
+echo "Thanks, you entered the name $name" >> output.txt
+echo "echo "Thanks, you entered the name $name" >> output.txt" >> 20202107005.run
+
