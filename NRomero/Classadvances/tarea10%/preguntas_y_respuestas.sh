0bc40b488e7ce8edeeb18aa916a80c2d01be49d1
Zeon556g <ncromeror.correo.udistrital.edu.co>
2021-10-02 02:02:03 -0500

    subiendo avance

---
+++ NRomero/Classadvances/tarea10%/preguntas_y_respuestas.sh
@@ -0,0 +1,9 @@
+#Author: Norman Romero
+#version: 0.01
+#Date: 2/oct/2021
+echo "Bienvenidos al jugo de preguntas y respuestas"
+echo "si aciertas todas las respuestas.. te dare el titulo de maestro"
+echo "cual es el nombre del cantante actual de la banda Iron Maiden"
+echo "A)Bruce Dickinson B)Michel Kiske C)Geoff Tate
+echo "selecione su respuesta escribiendo  A o  B o C"
+
