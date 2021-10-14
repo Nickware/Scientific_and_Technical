b3ae0126a837bd98d6ad277cb852d4c76dfa4704
matorress <matorress@correo.udistrital.edu.co>
2021-10-13 22:43:08 -0500

    agregando archivo pdf del algoritmo a la carpeta preprueba

--- MTorres/preprueba/figuras.sh
+++ MTorres/preprueba/figuras.sh
@@ -1,5 +1,15 @@
-echo "Se presentaran las siguientes figuras"
-echo "Un rectangulo, un ovalo, una flecha y un rombo"
+#!/usr/bin/bash
+# Authors: Norman Romero, Julian Aros, Marcos Torres
+# Title: Programa Figuras
+# Version: 0.1.0
+# Date: 13/10/2021 
+
+echo "=========================================================================================="
+echo "======================== Bienvenido al programa figuras =================================="
+echo "=========================================================================================="
+echo " "
+echo "A continuacion se le presentaran, en caso de que asi lo decida, las siguientes figuras: "
+echo "Un rectangulo, un ovalo, una flecha y un rombo respectivamente"
 echo " "
 echo "Desea ver las figuras: S / N ?"
 read ans
